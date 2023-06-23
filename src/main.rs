b2d146d90f0917a0148229e84af0f93686c7a752
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-23 11:32:35 +0545

    Ignore invalid inputs

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,9 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is {secret_number}");
-
-    let total_guesses = 5;
-    for num_guesses in 1..=total_guesses {
+    let mut num_guesses = 5;
+    loop {
+        println!("Remaining guesses: {}", num_guesses);
         println!("Please input your guess.");
 
         let mut guess = String::new();
@@ -20,9 +19,13 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: i32 = guess.trim().parse().expect("Please input a valid number");
-
-        println!("You guessed: {guess}");
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number");
+                continue;
+            }
+        };
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small"),
@@ -32,6 +35,11 @@ fn main() {
                 break;
             }
         }
-        println!("Remaining guesses: {}", total_guesses - num_guesses);
+
+        num_guesses -= 1;
+        if num_guesses == 0 {
+            println!("Uh-oh! No more guesses remaining. You lose.");
+            break;
+        }
     }
 }
