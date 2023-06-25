6f297997b7488c025731da9eb26d86ee1e02f751
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-25 15:46:45 +0545

    Demo shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,22 @@ fn main() {
     println!("The value of x = {x}");
     x = 5;
     println!("The value of x = {x}");
+
+    // shadowing
+    let y = 10;
+
+    let y = y + 1;
+
+    {
+        let y = y * 2;
+        println!("The value of y inside this scope is: {y}");
+    }
+
+    println!("The value of y outside is: {y}");
+
+    // shadowing types
+    let spaces = "    ";
+    let spaces = spaces.len();
+
+    println!("The number of spaces is {spaces}");
 }
