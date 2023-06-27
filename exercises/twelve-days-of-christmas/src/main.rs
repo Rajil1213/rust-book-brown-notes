8e6c92e7c1b07b347b35c5d0ac81810d8d3b0d65
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-27 17:00:20 +0545

    refactor: use macro to add \n to lines

--- exercises/twelve-days-of-christmas/src/main.rs
+++ exercises/twelve-days-of-christmas/src/main.rs
@@ -1,3 +1,20 @@
+/**
+This macro accepts an `expr` and returns it by appending a newline to it.
+
+The `expr` should thus be a str but does not have to be a reference to it
+as the macro takes the reference to the passed expression.
+
+Example:
+```
+assert_eq!(with_newline!("abc"), "abc\n");
+```
+*/
+macro_rules! with_newline {
+    ($line:expr) => {
+        &format!("{}\n", &$line)
+    };
+}
+
 fn main() {
     const DAY: [&str; 12] = [
         "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eigth", "ninth",
@@ -25,16 +42,14 @@ fn main() {
         let first_line: String =
             format!("On the {} day of Christmas my true love sent to me", DAY[i]);
 
-        let mut verse: String = String::from(first_line.to_owned() + "\n");
+        let mut verse: String = String::from(with_newline!(first_line));
 
         // go over the lines in reverse order for each line `i`
         for j in (0..=i).rev() {
-            verse.push_str(LINES[j]);
-            verse.push_str("\n");
+            verse.push_str(with_newline!(LINES[j]));
         }
 
-        lyrics.push_str(&verse.to_owned());
-        lyrics.push_str("\n")
+        lyrics.push_str(with_newline!(verse));
     }
 
     print!("{}", lyrics);
