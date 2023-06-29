af007e7895558fb6636c08b5e258d88862f14806
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-29 12:30:50 +0545

    Add slice test and separate modules in output

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,7 +1,11 @@
 mod ownership;
 mod references_and_borrowing;
+mod slices;
 
 fn main() {
     ownership::test();
+    println!("------------------------------");
     references_and_borrowing::test();
+    println!("------------------------------");
+    slices::test();
 }
