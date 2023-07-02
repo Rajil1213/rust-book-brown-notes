e1df1044176906f4d887d1bc326a3fe61b54437f
Rajil Bajracharya <imrb1213@gmail.com>
2023-07-02 23:17:21 +0545

    Create associated functions

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -22,6 +22,36 @@ fn main() {
         width: 20u32,
     };
 
+    let another_rect = &Rect {
+        height: 10,
+        width: 5,
+    };
+
     let area = rectangle::calculate_area(rect);
-    println!("Area of the rectange {:?} = {area}", rect);
+    println!("Area of the rectange {:?} = {area} (from function)", rect);
+    println!(
+        "Area of the rectange {:?} = {} (from method)",
+        rect,
+        rect.area()
+    );
+
+    println!(
+        "{:?} can hold {:?} ? {}",
+        rect,
+        another_rect,
+        rect.can_hold(another_rect)
+    );
+    println!(
+        "{:?} can hold {:?} ? {}",
+        another_rect,
+        rect,
+        another_rect.can_hold(rect)
+    );
+
+    let square_from_rect = Rect::square(10);
+    println!(
+        "Area of the square {:?} is {}",
+        square_from_rect,
+        square_from_rect.area()
+    );
 }
--- demo/src/rectangle.rs
+++ demo/src/rectangle.rs
@@ -4,6 +4,30 @@ pub struct Rect {
     pub height: u32,
 }
 
+// methods are defined within this impl block
+impl Rect {
+    // &self is short for self: &self
+    // self is an alias for the type impl block is for
+    // in this case `Rect`
+    pub fn area(&self) -> u32 {
+        self.height * self.width
+    }
+
+    pub fn can_hold(&self, other: &Rect) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
+}
+
+impl Rect {
+    // associated fn that is **not** a method
+    pub fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 pub fn calculate_area(rect: &Rect) -> u32 {
     rect.height * rect.width
 }
