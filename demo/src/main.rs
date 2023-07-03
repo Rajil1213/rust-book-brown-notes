a060a03804924d7ab34e5409be5d6a7a04eb6e48
Rajil Bajracharya <imrb1213@gmail.com>
2023-07-03 10:34:45 +0545

    Create demo for enums

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    #[derive(Debug)]
+    enum IpAddrKind {
+        V4(u8, u8, u8, u8),
+        V6(String),
+    }
+
+    let home = IpAddrKind::V4(127, 0, 0, 1);
+    let loopback = IpAddrKind::V6(String::from("::1"));
+
+    println!("Home = {:?}, Loopback = {:?}", home, loopback);
 }
