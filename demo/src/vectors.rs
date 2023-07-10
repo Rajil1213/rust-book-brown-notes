30f110460dca52a6241e6e8f86d695f6dd624621
Rajil Bajracharya <imrb1213@gmail.com>
2023-07-10 11:30:43 +0545

    Demonstrate vector iteration

--- demo/src/vectors.rs
+++ demo/src/vectors.rs
@@ -4,11 +4,25 @@ pub fn test() {
     v.push(4);
 
     let fourth = &v[3];
+    // v.push(5) // would not work here as mutable borrow occurs here to potentially reallocate the vector
     println!("The fourth element via index is {fourth}");
 
     let third = v.get(2);
     match third {
         Some(third) => println!("The third element via `get` is {third}"),
         None => println!("There is no third element :("),
+    }
+
+    // iterating through a vector, immutably
+    for v_ref in &v {
+        let value_plus_one = *v_ref + 1;
+        println!("v_ref + 1 = {v_ref} + 1 = {value_plus_one} ");
+    }
+
+    // iterating through a vector, mutably
+    for v_ref in &mut v {
+        print!("v_ref + 10 = {v_ref} + 10 = ");
+        *v_ref += 10;
+        println!("v_ref = {v_ref}");
     }
 }
