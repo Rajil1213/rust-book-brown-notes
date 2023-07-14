12c6c563fdcc82c308e8688dad5c022fccea3382
Rajil Bajracharya <imrb1213@gmail.com>
2023-07-14 11:46:04 +0545

    Demo errro propagation

--- demo/src/recoverable.rs
+++ demo/src/recoverable.rs
@@ -1,4 +1,7 @@
-use std::{fs::File, io::ErrorKind};
+use std::{
+    fs::File,
+    io::{Error, ErrorKind, Read},
+};
 
 fn try_to_open_file(path: &str) -> File {
     let file_result = File::open(path);
@@ -28,9 +31,42 @@ fn try_to_open_file_with_expect(path: &str) -> File {
     File::open(path).expect(&format!("{path} must exist in this project"))
 }
 
+fn read_username_from_file_explicit(path: &str) -> Result<String, Error> {
+    let mut username_file = match File::open(path) {
+        Ok(f) => f,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    if let Err(e) = username_file.read_to_string(&mut username) {
+        Err(e)
+    } else {
+        Ok(username)
+    }
+}
+
+fn read_username_from_file(path: &str) -> Result<String, Error> {
+    let mut username_file = File::open(path)?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
 pub fn test() {
     let creatable_file_path = "non_existing_file.txt";
     let unreadable_file_path = "unreadable.txt";
+    let contentful_file = "file_has_content.txt";
+
+    match read_username_from_file_explicit(contentful_file) {
+        Ok(username) => println!("username in file: {username}"),
+        Err(e) => println!("could not read from file: {contentful_file}: {e:?}"),
+    }
+
+    match read_username_from_file(contentful_file) {
+        Ok(username) => println!("username in file: {username}"),
+        Err(e) => println!("could not read from file: {contentful_file}, {e:?}"),
+    }
 
     try_to_open_file_with_expect(creatable_file_path);
     try_to_open_file_with_unwrap(creatable_file_path);
