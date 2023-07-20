cdb83ad388c88592f9daa6173d62a16109c81d65
Rajil Bajracharya <imrb1213@gmail.com>
2023-07-20 14:28:07 +0545

    Demo all features

--- demo/src/lifetimes.rs
+++ demo/src/lifetimes.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 fn longer<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
@@ -21,6 +23,18 @@ impl<'a> ImportantExcerpt<'a> {
     }
 }
 
+fn announce_the_longer<'a, T>(x: &'a str, y: &'a str, announcement: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement: {}", announcement);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 pub fn test() {
     let longer_string;
     let string1 = String::from("hello,");
@@ -41,4 +55,10 @@ pub fn test() {
         i.level(),
         i.announce_and_return_part(&announcement)
     );
+
+    let first_string = String::from("first string");
+    let second_string = String::from("second string");
+
+    let longer_string = announce_the_longer(&first_string, &second_string, "announcement");
+    println!("{}", longer_string);
 }
