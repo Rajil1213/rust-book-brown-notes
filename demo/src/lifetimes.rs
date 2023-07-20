b19f27652e0c93e985a3701509e68a30d9a85920
Rajil Bajracharya <imrb1213@gmail.com>
2023-07-20 13:55:04 +0545

    Demo struct lifetimes

--- demo/src/lifetimes.rs
+++ demo/src/lifetimes.rs
@@ -6,6 +6,10 @@ fn longer<'a>(x: &'a str, y: &'a str) -> &'a str {
     }
 }
 
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
 pub fn test() {
     let longer_string;
     let string1 = String::from("hello,");
@@ -13,4 +17,10 @@ pub fn test() {
 
     longer_string = longer(&string1, &string2);
     println!("The longer string is {longer_string}");
+
+    let novel = String::from("it was the best of time, it was the worst of ...");
+    let first_part = novel.split(',').next().expect("could not find a ','");
+    let i = ImportantExcerpt { part: first_part };
+
+    println!("First part = {}", i.part);
 }
