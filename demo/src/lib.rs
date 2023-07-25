e9ba6d46b8062c0543644c438391860de2f65650
Rajil Bajracharya <imrb1213@gmail.com>
2023-07-25 10:32:22 +0545

    Add sample tests (one of which fails)

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -4,11 +4,14 @@ pub fn add(left: usize, right: usize) -> usize {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn exploration() {
+        assert_eq!(2 + 2, 4)
+    }
+
+    #[test]
+    fn another() {
+        panic!("this test fails!");
     }
 }
