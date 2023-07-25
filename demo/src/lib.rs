27d14b4f4fb5d0fde9dfa256d7c3bbb1c7aedd51
Rajil Bajracharya <imrb1213@gmail.com>
2023-07-25 12:28:39 +0545

    Add tests

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -1,17 +1,79 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
+pub fn panic_if_greater(input: i32, limit: i32) -> String {
+    if input > limit {
+        panic!("Input cannot be greater than {limit}, received {input}");
+    }
+
+    format!("Congratulations, you stayed under the limit, received {input}!")
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
 
     #[test]
-    fn exploration() {
-        assert_eq!(2 + 2, 4)
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+
+        let smaller = Rectangle {
+            width: 7,
+            height: 6,
+        };
+
+        assert!(larger.can_hold(&smaller));
     }
 
     #[test]
-    fn another() {
-        panic!("this test fails!");
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+
+        let smaller = Rectangle {
+            width: 7,
+            height: 6,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        // should return "Hello, Carol"
+        // greeting (`Hello`) may change, so only test input
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain the name, value was {}",
+            result
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn greater_than_limit() {
+        let value = 60;
+        let limit = 40;
+
+        panic_if_greater(value, limit);
     }
 }
