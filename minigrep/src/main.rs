64b0cb65b5be6fc29ecf068718e7defaaf2b1272
Rajil Bajracharya <imrb1213@gmail.com>
2023-07-27 12:00:54 +0545

    refactor: separate env args parsing logic

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,12 +2,16 @@ use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    let searchstring = &args[1];
-    let filepath = &args[2];
-
+    let (searchstring, filepath) = parse_args(&args);
     let contents = fs::read_to_string(filepath).expect(&format!("{} does not exist", filepath));
 
     println!("searchstring = {searchstring}, filepath = {filepath}");
     println!("contents:\n{}", contents);
+}
+
+fn parse_args(args: &[String]) -> (&str, &str) {
+    let searchstring = &args[1];
+    let filepath = &args[2];
+
+    (searchstring, filepath)
 }
