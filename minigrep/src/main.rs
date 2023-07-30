894a4725c7258694b795cbd8a475ed9fd845aadc
Rajil Bajracharya <imrb1213@gmail.com>
2023-07-30 20:59:52 +0545

    Direct errors to stderr

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,13 +4,13 @@ use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = minigrep::Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     // use if..let because there is nothing to unwrap
     if let Err(err) = minigrep::run(config) {
-        println!("Application error: {err}");
+        eprintln!("Application error: {err}");
         process::exit(1);
     };
 }
