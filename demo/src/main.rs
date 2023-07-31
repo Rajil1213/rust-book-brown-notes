f3aa6cf1d02018702a80722079c7842f191d05cc
Rajil Bajracharya <imrb1213@gmail.com>
2023-07-31 11:10:21 +0545

    Demo closure with unwrap_or_else

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,3 +1,14 @@
+use demo::{Inventory, ShirtColor};
+
 fn main() {
-    println!("Hello, world!");
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!("The user with preference {user_pref1:?} gets {giveaway1:?}");
+
+    let giveaway2 = store.giveaway(None);
+    println!("The user with no preference gets {giveaway2:?}");
 }
