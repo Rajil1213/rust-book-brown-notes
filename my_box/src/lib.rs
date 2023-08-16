0bc5a7ecec5579b64ac19bc29adc62dfc23c7811
Rajil Bajracharya <imrb1213@gmail.com>
2023-08-16 11:32:48 +0545

    Implement custom smart pointer with Deref trait

--- my_box/src/lib.rs
+++ my_box/src/lib.rs
@@ -1,9 +1,47 @@
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 #[cfg(test)]
 mod test {
+    use super::*;
+
     #[test]
     fn test_ref() {
         let x = 5;
         let y = &x;
+
+        assert_eq!(5, x);
+        assert_eq!(x, *y);
+    }
+
+    #[test]
+    fn test_box_ref() {
+        let x = 5;
+        let y = Box::new(x);
+
+        assert_eq!(5, x);
+        assert_eq!(x, *y);
+    }
+
+    #[test]
+    fn test_mybox_ref() {
+        let x = 5;
+        let y = MyBox::new(x);
 
         assert_eq!(5, x);
         assert_eq!(x, *y);
