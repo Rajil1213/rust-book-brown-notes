cac00f2cfb5cc498e535c557efe7f6ace730fb2e
Rajil Bajracharya <imrb1213@gmail.com>
2023-08-17 10:48:58 +0545

    Use Rc<T> instead of Box<T>

--- cons/src/lib.rs
+++ cons/src/lib.rs
@@ -1,5 +1,13 @@
+use std::rc::Rc;
+
 #[derive(Debug)]
 pub enum List {
     Cons(i32, Box<List>),
+    Nil,
+}
+
+#[derive(Debug)]
+pub enum RcList {
+    Cons(i32, Rc<RcList>),
     Nil,
 }
--- cons/src/main.rs
+++ cons/src/main.rs
@@ -1,6 +1,31 @@
-use cons::List::{Cons, Nil};
+use std::rc::Rc;
+
+use cons::{
+    List::{Cons, Nil},
+    RcList::Nil as RcNil,
+    RcList::{self, Cons as RcCons},
+};
 
 fn main() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     println!("List: {list:?}");
+
+    // define a new instance of RcList
+    let a: RcList = RcCons(5, Rc::new(RcCons(10, Rc::new(RcNil))));
+    // convert to Rc<T> pointing to `a`, reference count to `a` becomes 1
+    let rca: Rc<RcList> = Rc::new(a);
+    println!("count after creating rca = {}", Rc::strong_count(&rca));
+
+    // clone rca `Rc<RcList>`, to increase reference count to `a` from 1 to 2
+    let b: RcList = RcCons(3, Rc::clone(&rca));
+    println!("count after creating b = {}", Rc::strong_count(&rca));
+    {
+        // clone rca `Rc<RcList>` again, to increase reference count to `a` from 2 to 3
+        let c: RcList = RcCons(4, Rc::clone(&rca));
+        println!("count after creating c = {}", Rc::strong_count(&rca));
+    }
+    println!(
+        "count after c goes out of scope = {}",
+        Rc::strong_count(&rca)
+    );
 }
