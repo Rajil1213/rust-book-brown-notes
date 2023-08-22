9eb262479c6cd39010758c295c28657c9f11723a
Rajil Bajracharya <imrb1213@gmail.com>
2023-08-22 15:53:39 +0545

    Create initial implementation for Blog

---
+++ blog/src/lib.rs
@@ -0,0 +1,38 @@
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
+    }
+}
+
+trait State {}
+struct Draft {}
+
+impl State for Draft {}
+
+#[cfg(test)]
+mod blog {
+    use super::Post;
+
+    #[test]
+    fn only_allows_approved_content_to_be_posted() {
+        let mut post = Post::new();
+        let content = "I ate a salad for lunch today";
+
+        post.add_text(content);
+        assert_eq!("", post.content());
+
+        post.request_review();
+        assert_eq!("", post.content());
+
+        post.approve();
+        assert_eq!(content, post.content());
+    }
+}
