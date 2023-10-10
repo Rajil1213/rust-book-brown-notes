bbec9b54f4024167b02f6a32f8c426cfb0748a87
Rajil Bajracharya <imrb1213@gmail.com>
2023-10-10 11:20:17 +0545

    feat: demo generics in traits and op overloading

--- traits/src/lib.rs
+++ traits/src/lib.rs
@@ -17,6 +17,18 @@ impl Add for Point {
     }
 }
 
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, rhs: Meters) -> Self::Output {
+        Millimeters(self.0 + rhs.0 * 1000)
+    }
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -28,5 +40,10 @@ mod tests {
             Point { x: 1, y: 2 } + Point { x: 2, y: 1 },
             Point { x: 3, y: 3 }
         );
+    }
+
+    #[test]
+    fn adding_meters_to_millimeters_works() {
+        assert_eq!(Millimeters(2000) + Meters(2), Millimeters(4000));
     }
 }
