6ecf9d77c261ef3419886748db503fb597fffa32
Rajil Bajracharya <imrb1213@gmail.com>
2023-10-23 15:13:56 +0545

    feat: demo unsafe functions

--- safetyoff/src/lib.rs
+++ safetyoff/src/lib.rs
@@ -1,4 +1,4 @@
-fn unsafe_mem() -> (i32, i32) {
+pub fn unsafe_mem() -> (i32, i32) {
     let mut num = 5;
     let r1 = &num as *const i32;
     let r2 = &mut num as *mut i32;
@@ -6,8 +6,17 @@ fn unsafe_mem() -> (i32, i32) {
     unsafe {
         println!("const unsafe ref = {}", *r1);
         println!("mut ref = {}", *r2);
-        return (*r1, *r2);
+        (*r1, *r2)
     }
+}
+
+/// Does something unsafe.
+///
+/// # Safety
+///
+/// Use at your own risk. 😈
+pub unsafe fn dangerous() -> Result<(), String> {
+    Ok(())
 }
 
 #[cfg(test)]
@@ -18,5 +27,12 @@ mod tests {
     fn test_unsafe_mem() {
         assert_eq!(5, unsafe_mem().0);
         assert_eq!(5, unsafe_mem().1);
+    }
+
+    #[test]
+    fn call_dangerous() {
+        unsafe {
+            assert!(dangerous().is_ok());
+        }
     }
 }
